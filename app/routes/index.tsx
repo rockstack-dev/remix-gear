79ff9e9c0b547a87281999d8247166beb471d7c5
AlexandroMtzG <alex.martinez@absys.com.mx>
2024-08-02 15:53:49 -0600

    guide: form actions

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -98,6 +98,25 @@ export default function () {
               </Link>
             </div>
           </div>
+
+          <div className="space-y-2 pt-12">
+            <h3 className="text-sm font-bold">Quick guides</h3>
+            <div className="grid grid-cols-3 gap-4">
+              <Link
+                to="/forms"
+                className="rounded-md border border-border bg-background p-3 text-primary hover:bg-secondary/90 hover:text-secondary-foreground"
+              >
+                <div className="flex justify-center text-sm font-medium">Form actions</div>
+              </Link>
+              <Link
+                to="https://twitter.com/AlexandroMtzG"
+                target="_blank"
+                className="rounded-md border border-dashed border-border bg-background p-3 text-primary opacity-50 hover:bg-secondary/90 hover:text-secondary-foreground"
+              >
+                <div className="flex justify-center text-sm font-medium">Let me know!</div>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
